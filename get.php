4b1f798a88492cc9ba0b7304bb02642e81db6fbf
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-09-06 14:08:30 -0500

    Update get.php

--- get.php
+++ get.php
@@ -2,7 +2,10 @@
 $pageTitle = "Get";
 include "view-header.php";
 ?>
-      <h1>Home</h1>
+      <h1>Post</h1>
+      <form method="get" action="post-results.php">
+        <input type="text" name="my-name">
+        <input type="submit" value="Post data">
 <?php
 include "view-footer.php";
 ?>
