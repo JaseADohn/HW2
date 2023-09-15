6b69c013dcbf1cea6fda873f5bc8ea83a5a21293
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-09-15 12:24:20 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -10,7 +10,7 @@ include "view-footer.php";
 function getDisplay(){
       
 if (isset($_POST['my-name'])) {
-return "<p>Te value ent is:</p>". $_POST['my-name'];
+return "<p>The value ent is:</p>". $_POST['my-name'];
 }else {
 return "<p>Nothing posted to the page.</p>";
 }
