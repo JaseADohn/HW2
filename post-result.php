c10da266b3cea7bade57519c0bea8cde09994493
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-09-15 12:28:21 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -12,7 +12,7 @@ function getDisplay(){
 if (isset($_POST['my-name'])) {
 return "<p>The value sent is:</p>". $_POST['my-name'];
 }else {
-return "<p>Nothing posted to the page.</p>";
+return "<p>Nothing seems to be posted on this page.</p>";
 }
       
 }
